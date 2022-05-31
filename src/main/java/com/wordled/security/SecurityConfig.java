19a864d0de263959dd521704579022bdf81f7e6e
JT <59325232+jfemrite@users.noreply.github.com>
2022-05-31 15:03:59 -0500

    Update SecurityConfig.java

--- src/main/java/com/wordled/security/SecurityConfig.java
+++ src/main/java/com/wordled/security/SecurityConfig.java
@@ -19,84 +19,77 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private static final String LOGIN_PROCESSING_URL = "/login";
-    private static final String LOGIN_FAILURE_URL = "/login?error";
-    private static final String LOGIN_URL = "/login";
-    private static final String LOGOUT_SUCCESS_URL = "/login";
+  private static final String LOGIN_FAILURE_URL = "/login?error";
+  private static final String LOGIN_URL = "/login";
+  private static final String LOGOUT_SUCCESS_URL = "/login";
 
-    @Override
-    protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
-        // authentication manager (see below)
-    }
+  /**
+   * Require login to access internal pages and configure login form.
+   */
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    // Vaadin handles CSRF internally
+    http.csrf().disable()
 
-    /**
-     * Require login to access internal pages and configure login form.
-     */
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        // Vaadin handles CSRF internally
-        http.csrf().disable()
+        // Register our CustomRequestCache, which saves unauthorized access attempts, so the user is redirected after login.
+        .requestCache().requestCache(new CustomRequestCache())
 
-                // Register our CustomRequestCache, which saves unauthorized access attempts, so the user is redirected after login.
-                .requestCache().requestCache(new CustomRequestCache())
+        // Restrict access to our application.
+        .and().authorizeRequests()
 
-                // Restrict access to our application.
-                .and().authorizeRequests()
+        // Allow all Vaadin internal requests.
+        .requestMatchers(SecurityUtils::isFrameworkInternalRequest).permitAll()
 
-                // Allow all Vaadin internal requests.
-                .requestMatchers(SecurityUtils::isFrameworkInternalRequest).permitAll()
+        // Allow all requests by logged-in users.
+        .anyRequest().authenticated()
 
-                // Allow all requests by logged-in users.
-                .anyRequest().permitAll()
+        // Configure the login page.
+        .and().formLogin()
+        .loginPage(LOGIN_URL).permitAll()
+        .loginProcessingUrl(LOGIN_PROCESSING_URL)
+        .failureUrl(LOGIN_FAILURE_URL)
 
-                // Configure the login page.
-                .and().formLogin()
-                .loginPage(LOGIN_URL).permitAll()
-                .loginProcessingUrl(LOGIN_PROCESSING_URL)
-                .failureUrl(LOGIN_FAILURE_URL)
+        // Configure logout
+        .and().logout().logoutSuccessUrl(LOGOUT_SUCCESS_URL);
+  }
 
-                // Configure logout
-                .and().logout().logoutSuccessUrl(LOGOUT_SUCCESS_URL);
-    }
-    
-    @Bean
-    @Override
-    public UserDetailsService userDetailsService() {
-        UserDetails user = User.withUsername("user")
-                .password("{noop}userpass")
-                .roles("USER")
-                .build();
+  @Bean
+  @Override
+  public UserDetailsService userDetailsService() {
+    UserDetails user = User.withUsername("user")
+            .password("{noop}userpass")
+            .roles("USER")
+            .build();
 
-        return new InMemoryUserDetailsManager(user);
-    }
+    return new InMemoryUserDetailsManager(user);
+  }
 
-    /**
-     * Allows access to static resources, bypassing Spring Security.
-     */
-    @Override
-    public void configure(WebSecurity web) {
-        web.ignoring().antMatchers(
-                // Client-side JS
-                "/VAADIN/**",
+  /**
+   * Allows access to static resources, bypassing Spring Security.
+   */
+  @Override
+  public void configure(WebSecurity web) {
+    web.ignoring().antMatchers(
+        // Client-side JS
+        "/VAADIN/**",
 
-                // the standard favicon URI
-                "/favicon.ico",
+        // the standard favicon URI
+        "/favicon.ico",
 
-                "/register",
+        // the robots exclusion standard
+        "/robots.txt",
 
-                // the robots exclusion standard
-                "/robots.txt",
+        // web application manifest
+        "/manifest.webmanifest",
+        "/sw.js",
+        "/offline.html",
 
-                // web application manifest
-                "/manifest.webmanifest",
-                "/sw.js",
-                "/offline.html",
+        // icons and images
+        "/icons/**",
+        "/images/**",
+        "/styles/**",
 
-                // icons and images
-                "/icons/**",
-                "/images/**",
-                "/styles/**",
-
-                // (development mode) H2 debugging console
-                "/h2-console/**");
-    }
+        // (development mode) H2 debugging console
+        "/h2-console/**");
+  }
 }
